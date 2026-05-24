84bf7564e77cdfb749a567bc125671425ac3cf81
pharezvarlack <varlack17@hotmail.com>
2026-05-24 14:26:53 -0400

    refacor: move getting spreadsheet id to function

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ from get_and_process_data import getAndProcessData
 from write_to_sheets import writeToSheets
 from google_sheets_service import GoogleSheets
 from google_drive_service import GoogleDrive
-# TO DO
-# handle creating a spreadsheet when none exists
-# auto get date range
+from datetime import datetime
+
+def get_spread_sheet_id(driveService):
+    now = datetime.now()
+    year = now.year
+    month = now.strftime("%B")
+
+    folder_id = driveService.find_folder_by_name(year)
+    file_id = driveService.find_file_by_name(month, folder_id)
+    return file_id
+
 def main():
     sheetsService = GoogleSheets()
-    driveService = GoogleDrive()
-
+    driveService = GoogleDrive()    
+                     
     data = getAndProcessData()
-    folder_id = driveService.find_folder_by_name("2026")
-    file_id = driveService.find_file_by_name("May", folder_id)
+
+    file_id = get_spread_sheet_id(driveService)
     
     writeToSheets(sheetsService, data, file_id)
     print("Done")
